4ce200507a64c00d7538014f7900bbce8da85af4
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-06-25 23:23:56 +0530

    chore: add files

---
+++ src/main/java/com/sg/interview_creation_portal/InterviewCreationPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sg.interview_creation_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCreationPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewCreationPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sg/interview_creation_portal/InterviewCreationPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.interview_creation_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewCreationPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
