9d553551690fad2ba0318aa9b84ea0d21d83e37a
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-06-26 10:35:15 +0530

    fix: network calls

--- mobile_app/lib/src/repository/interviewer_repository.dart
+++ mobile_app/lib/src/repository/interviewer_repository.dart
@@ -5,7 +5,7 @@ import 'package:interview_app/src/repository/network_config.dart';
 class _InterviewerRepository {
   Client _client = Client();
 
-  Future<Response> getInterviewees() async {
+  Future<Response> getInterviewers() async {
     return await _client.get(
       Uri.parse('$baseURL/interviewer/all'),
     );
