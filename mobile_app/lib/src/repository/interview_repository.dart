81e7e4f6c3a4632f4cced4a828863f1ceb9f0bd3
Saarthak Gupta <saarthakgupta08@gmail.com>
2021-06-26 10:32:09 +0530

    fix: fix network calls

--- mobile_app/lib/src/repository/interview_repository.dart
+++ mobile_app/lib/src/repository/interview_repository.dart
@@ -15,12 +15,39 @@ class _InterviewRepository {
   Future<Response> scheduleInterview(int startTiming, int endTiming,
       int interviewerId, int intervieweeId) async {
     return await _client.post(
-      Uri.parse('$baseURL/interview/all'),
+      Uri.parse('$baseURL/interview/'),
       body: jsonEncode(<String, dynamic>{
         'startTiming': startTiming,
         'endTiming': endTiming,
         'interviewerId': interviewerId,
         'intervieweeId': intervieweeId
+      }),
+    );
+  }
+
+  Future<Response> modifyInterview(int startTiming, int endTiming,
+      int interviewerId, int intervieweeId, int id) async {
+    return await _client.put(
+      Uri.parse('$baseURL/interview/'),
+      body: jsonEncode(<String, dynamic>{
+        'startTiming': startTiming,
+        'endTiming': endTiming,
+        'interviewerId': interviewerId,
+        'intervieweeId': intervieweeId,
+        'id': id
+      }),
+    );
+  }
+
+  Future<Response> checkAvailability(int startTiming, int endTiming,
+      int interviewerId, int intervieweeId, int id) async {
+    return await _client.post(
+      Uri.parse('$baseURL/interview/check'),
+      body: jsonEncode(<String, dynamic>{
+        'startTiming': startTiming,
+        'endTiming': endTiming,
+        'interviewerId': interviewerId,
+        'intervieweeId': intervieweeId,
       }),
     );
   }
